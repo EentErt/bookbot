b1afd014e3a5694eb844ffaa161f82647bedd017
natenatenatenatenatenatenatenate <nate.giff@gmail.com>
2025-05-12 11:54:40 -0700

    beautify report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import character_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,7 +10,16 @@ def get_book_text(path_to_file):
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
     word_count = count_words(file_contents)
+    letter_count = character_count(file_contents)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"{word_count} words found in the document")
+    print("--------- Character Count -------")
+    for letter in letter_count:
+        print(f"{letter["char"]}: {letter["num"]}")
+    print("============= END ===============")
     pass
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,36 @@ def count_words(text):
     # the count is the length of the list given by the split function
     count = len(word_list)
     return count
+
+def character_count(text):
+    # initialize the letter dictionary
+    letter_count = {}
+
+    # count the letters
+    for character in text:
+        character = character.lower()
+        if character not in letter_count:
+            letter_count[character] = 0
+        letter_count[character] += 1
+
+    report = character_count_report(letter_count)
+    return report
+
+def character_count_report(character_dict):
+    report = []
+
+    for character in character_dict:
+        if character.isalpha():
+            dict = {}
+            dict["char"] = character
+            dict["num"] = character_dict[character]
+            report.append(dict)
+
+    report.sort(reverse = True, key = sort_on)
+    return report
+
+def sort_on(dict):
+    return dict["num"]
+
+
+    # go through the characters and add them to the dictionary
